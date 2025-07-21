e11cde7f4c3a5098fd9670382ce8718ed3d42518
masteringsteve <masteringsteve@gmail.com>
2025-07-20 22:46:25 -0300

    fdvcfdss

--- src/database.js
+++ src/database.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   max: 20, // maximum number of clients in the pool
   idleTimeoutMillis: 30000, // close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // return an error after 2 seconds if connection could not be established
+  ssl: DATABASE_URL && DATABASE_URL.includes('sslmode=require') ? false : { rejectUnauthorized: false }
 });
 
 // Initialize database
